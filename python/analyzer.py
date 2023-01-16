df724651b5794e22cfbd6d73a230527623eb845e
Rocky <jornolieslagers@gmail.com>
2023-01-16 10:39:46 +0100

    Update analyzer.py
    -Work on the main program and functions

--- python/analyzer.py
+++ python/analyzer.py
@@ -3,26 +3,38 @@ from datetime import datetime
 import lib_bamboo as bamboo
 import os
 
-................... #Deze regel nog invullen! Hoe maak je het scherm leeg?
+totalViolations = 0
+totalMatches = 0
+
+os.system('cls') #Deze regel nog invullen! Hoe maak je het scherm leeg?
 print("Working...")
 
-data = pd.read_excel("python/Voorbeeld_Divisie.xlsx")
+data = pd.read_excel("../files/Basketbal_Talent League_tussenstand.xlsx")
 data["datum"] = pd.to_datetime(data["datum"], format="%d/%m/%Y")
 data = data.sort_values("datum")
 
 #Informatievraag 1
-
+for match in data:
+    totalMatches += 1
+    totalViolations += match['overtredingen']
+    file1 = open("..files/sum.txt", "w", encoding="utf-8")
+    file1.write(totalViolations)
+    file1.close()
 
 
 #Informatievraag 2
-
+averageViolations = totalViolations / totalMatches
+file2 = open("..files/average.txt", "w", encoding="utf-8")
+file2.write(averageViolations)
+file2.close()
 
 
 #Informatievraag 3
-zwartBoek = ....... #Deze regel nog invullen! Hoe maak je een top 5?
+data_sorted = data.sort_values('overtredingen', ascending=True)
+zwartBoek = data_sorted.head(10) #Deze regel nog invullen! Hoe maak je een top 5?
 file3 = open("files/zwartboek.txt", "w", encoding="UTF-8")
 file3.write(bamboo.prettify(zwartBoek, type="zwartboek"))
-................... #Deze regel nog invullen! Hoe sluit je file3?
+file3.close() #Deze regel nog invullen! Hoe sluit je file3?
 
 
 #Informatievraag 4
