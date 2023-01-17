08764acac9a5279075b0a97f3eea5cb001a846a5
Rocky <jornolieslagers@gmail.com>
2023-01-17 13:27:48 +0100

    Python Program
    -Fixed functionality
    -Debugged most of the program
    -Finished data handler

--- python/analyzer.py
+++ python/analyzer.py
@@ -3,48 +3,65 @@ from datetime import datetime
 import lib_bamboo as bamboo
 import os
 
-totalViolations = 0
-totalMatches = 0
-
-os.system('cls') #Deze regel nog invullen! Hoe maak je het scherm leeg?
+os.system("cls")
 print("Working...")
 
-data = pd.read_excel("test.xlsx")
+data = pd.read_excel("python/Basketbal_Talent League_tussenstand copy.xlsx")
 data["datum"] = pd.to_datetime(data["datum"], format="%d/%m/%Y")
 data = data.sort_values("datum")
 
-#Informatievraag 1
-for match in data:
-    totalMatches += 1
-    totalViolations += match['overtredingen']
-    file1 = open("..files/sum.txt", "w", encoding="utf-8")
-    file1.write(totalViolations)
-    file1.close()
+#Informatie-vraag 1
+totalFouls = data["overtredingen"].sum()
+file1 = open("files/sum.txt", "w", encoding="UTF-8")
+file1.write(str(totalFouls))
+file1.close()
 
-
-#Informatievraag 2
-averageViolations = totalViolations / totalMatches
-file2 = open("..files/average.txt", "w", encoding="utf-8")
-file2.write(averageViolations)
+#Informatie-vraag 2
+averageFouls = data["overtredingen"].mean()
+file2 = open("files/average.txt", "w", encoding="UTF-8")
+file2.write(str(round(averageFouls)))
 file2.close()
 
-
-#Informatievraag 3
-data_sorted = data.sort_values('overtredingen', ascending=True)
-zwartBoek = data_sorted.head(10) #Deze regel nog invullen! Hoe maak je een top 5?
+#Informatie-vraag 3
+zwartBoek = data.sort_values("overtredingen", ascending=False).head(5)
 file3 = open("files/zwartboek.txt", "w", encoding="UTF-8")
 file3.write(bamboo.prettify(zwartBoek, type="zwartboek"))
-file3.close() #Deze regel nog invullen! Hoe sluit je file3?
+file3.close()
+
+#Informatie-vraag 4
+filter1 = (data["datum"] > datetime(2020, 12, 21))
+ereGalerij = data[filter1]
+filter2 = (ereGalerij["overtredingen"] < 2)
+ereGalerij = ereGalerij[filter2]
+file4 = open("files/eregalerij.txt", "w", encoding="UTF-8")
+file4.write(bamboo.prettify(ereGalerij, type="eregalerij"))
+file4.close()
 
 
-#Informatievraag 4
+#Informatie-vraag 5a (EXCELLENT)
+data["datum"] = data["datum"].dt.strftime("%Y-%m")
+draaitabel_scheids = data.pivot_table(
+    index="scheidsrechter",
+    columns="datum",
+    values="overtredingen",
+    aggfunc=sum
+)
 
+file5a = open("files/draaitabel_scheids.txt", "w", encoding="UTF-8")
+file5a.write(bamboo.prettify(draaitabel_scheids))
+file5a.close()
 
+#Informatie-vraag 5b (EXCELLENT)
+draaitabel_stadion = data.pivot_table(
+    index="stadion",
+    columns="datum",
+    values="bezoekersaantal",
+    aggfunc=sum
+)
 
-
-
-
-
+file5b = open("files/draaitabel_stadion.txt", "w", encoding="UTF-8")
+file5b.write(bamboo.prettify(draaitabel_stadion))
+file5b.close()
 
 
 print("Done!")
